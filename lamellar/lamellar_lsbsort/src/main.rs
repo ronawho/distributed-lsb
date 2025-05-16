9d891bf6aac8fd957729b0133cebc2fe979898d8
Ryan D. Friese <ryan.friese@pnnl.gov>
2025-05-16 15:17:40 -0700

    some minor improvements to original version + cargo fmt

--- lamellar/lamellar_lsbsort/src/main.rs
+++ lamellar/lamellar_lsbsort/src/main.rs
@@ -1,17 +1,14 @@
-use rayon::prelude::*;
-use rand::prelude::*;
+#![allow(non_snake_case)]
 use rand::distr::StandardUniform;
+use rand::prelude::*;
 use rand_pcg::Pcg64;
 use std::time::Instant;
-use std::process::exit;
-use std::ptr;
-use std::slice;
 
 use lamellar::array::prelude::*;
 use lamellar::array::Distribution;
 
 mod prefix_sum_impl;
-use prefix_sum_impl::inclusive_prefix_sum;
+use prefix_sum_impl::exclusive_prefix_sum;
 
 const RADIX: u64 = 16; // TODO set to 16
 const N_DIGITS: u64 = 64 / RADIX;
@@ -19,115 +16,44 @@ const N_BUCKETS: u64 = 1 << RADIX;
 const COUNTS_SIZE: usize = N_BUCKETS as usize;
 const MASK: u64 = N_BUCKETS - 1;
 
-#[lamellar::AmData(
-    Default,
-    Debug,
-    ArrayOps(Arithmetic, CompExEps, Shift),
-    PartialEq,
-    PartialOrd
-)]
+#[lamellar::AmData(Default, Debug, ArrayOps, PartialEq, PartialOrd)]
 struct SortElement {
     key: u64,
     val: u64,
 }
 
-// These are currently necessary in order for it to compile.
-// It's likely that this is an aspect of Lamellar that could be improved.
-impl std::ops::AddAssign for SortElement {
-    fn add_assign(&mut self, other: Self) {
-        *self = Self {
-            key: self.key + other.key,
-            val: self.val + other.val,
-        }
-    }
-}
-
-impl std::ops::SubAssign for SortElement {
-    fn sub_assign(&mut self, other: Self) {
-        *self = Self {
-            key: self.key - other.key,
-            val: self.val - other.val,
-        }
-    }
-}
-
-impl std::ops::Sub for SortElement {
-    type Output = Self;
-    fn sub(self, other: Self) -> Self {
-        Self {
-            key: self.key - other.key,
-            val: self.val - other.val,
-        }
-    }
-}
-
-impl std::ops::MulAssign for SortElement {
-    fn mul_assign(&mut self, other: Self) {
-        *self = Self {
-            key: self.key * other.key,
-            val: self.val * other.val,
-        }
-    }
-}
-
-impl std::ops::DivAssign for SortElement {
-    fn div_assign(&mut self, other: Self) {
-        *self = Self {
-            key: self.key / other.key,
-            val: self.val / other.val,
-        }
-    }
-}
-
-impl std::ops::RemAssign for SortElement {
-    fn rem_assign(&mut self, other: Self) {
-        *self = Self {
-            key: self.key % other.key,
-            val: self.val % other.val,
-        }
-    }
-}
-
-impl std::ops::ShlAssign for SortElement {
-    fn shl_assign(&mut self, other: Self) {
-        self.key <<= other.key;
-        self.val <<= other.val;
-    }
-}
-
-impl std::ops::ShrAssign for SortElement {
-    fn shr_assign(&mut self, other: Self) {
-        self.key >>= other.key;
-        self.val >>= other.val;
-    }
-}
-
 fn get_bucket(x: &SortElement, d: u64) -> usize {
-    return ((x.key >> (RADIX*d)) & MASK) as usize;
+    return ((x.key >> (RADIX * d)) & MASK) as usize;
 }
 
-fn calc_global_index(bucket: usize, my_pe: usize, my_task: usize,
-                     num_pes: usize, n_tasks: usize) -> usize {
-    return (bucket*num_pes*n_tasks) + (my_pe*n_tasks) + my_task;
+fn calc_global_index(
+    bucket: usize,
+    my_pe: usize,
+    my_task: usize,
+    num_pes: usize,
+    n_tasks: usize,
+) -> usize {
+    return (bucket * num_pes * n_tasks) + (my_pe * n_tasks) + my_task;
 }
 
 // shuffle elements from A into B
-fn global_shuffle(A: &UnsafeArray::<SortElement>,
-                  B: &UnsafeArray::<SortElement>,
-                  digit: u64,
-                  world: &LamellarWorld,
-                  n_per_task: usize) {
-
+fn global_shuffle(
+    A: &UnsafeArray<SortElement>,
+    B: &UnsafeArray<SortElement>,
+    digit: u64,
+    world: &LamellarWorld,
+    n_per_task: usize,
+) {
     let my_pe = world.my_pe();
     let num_pes = world.num_pes();
     let n_tasks_per_pe = world.num_threads_per_pe();
-    let n_per_pe = n_tasks_per_pe * n_per_task;
     let n_tasks_total = num_pes * n_tasks_per_pe;
-    let n = n_per_task * n_tasks_total;
 
-    let counts_f = UnsafeArray::<i64>::new(world.team(),
-                                           COUNTS_SIZE*n_tasks_per_pe*num_pes,
-                                           Distribution::Block);
+    let counts_f = UnsafeArray::<i64>::new(
+        world.team(),
+        COUNTS_SIZE * n_tasks_per_pe * num_pes,
+        Distribution::Block,
+    );
     let counts = counts_f.block();
 
     // 'counts' stores the counts in the global order
@@ -145,29 +71,28 @@ fn global_shuffle(A: &UnsafeArray::<SortElement>,
     unsafe {
         // get a fresh reference to the same counts array
         let counts_clone = counts.clone();
-        let _ = A.local_chunks(n_per_task)
-                 .enumerate().for_each(move |(tid,task_slice)| {
+        let _ = A
+            .local_chunks(n_per_task)
+            .enumerate()
+            .for_each(move |(tid, task_slice)| {
+                //println!("tid {:?} got chunk {:?}", tid, task_slice);
+                // compute the count for this task / chunk
+                // note, vec! allocates a fixed-sized array on the heap
+                let mut this_task_counts = vec![0; COUNTS_SIZE];
+                for elt in task_slice {
+                    this_task_counts[get_bucket(elt, digit)] += 1;
+                }
+                // store the task count into the global count
+                let mut indices = vec![0; COUNTS_SIZE];
+                for i in 0..COUNTS_SIZE {
+                    let glob_count_idx = i * n_tasks_total + my_pe * n_tasks_per_pe + tid;
 
-                 //println!("tid {:?} got chunk {:?}", tid, task_slice);
-                 // compute the count for this task / chunk
-                 // note, vec! allocates a fixed-sized array on the heap
-                 let mut this_task_counts = vec![0; COUNTS_SIZE];
-                 for elt in task_slice {
-                     this_task_counts[get_bucket(elt, digit)] += 1;
-                 }
-                 // store the task count into the global count
-                 let mut indices = vec![0; COUNTS_SIZE];
-                 for i in 0..COUNTS_SIZE {
-                     let glob_count_idx = i*n_tasks_total +
-                                          my_pe*n_tasks_per_pe +
-                                          tid;
+                    indices[i] = glob_count_idx;
+                }
 
-                     indices[i] = glob_count_idx;
-                 }
-
-                 let _ =
-                   counts_clone.batch_store(indices, this_task_counts).spawn();
-               }).spawn();
+                let _ = counts_clone.batch_store(indices, this_task_counts).spawn();
+            })
+            .spawn();
         //A.wait_all();
         //counts.wait_all();
         world.wait_all();
@@ -190,8 +115,8 @@ fn global_shuffle(A: &UnsafeArray::<SortElement>,
 
     {
         // copy the counts (for use in computing the exclusive prefix sum)
-        let copy_counts = LocalLockArray::<i64>::new(world.team(), counts_lck.len(), Distribution::Block).block();
-        copy_counts.local_iter_mut().zip(counts_lck.local_iter()).for_each(move|(copy,count)| { *copy = *count; }).block();
+        // let copy_counts = LocalLockArray::<i64>::new(world.team(), counts_lck.len(), Distribution::Block).block();
+        // copy_counts.local_iter_mut().zip(counts_lck.local_iter()).for_each(move|(copy,count)| { *copy = *count; }).block();
 
         /*println!("copy_counts is");
         copy_counts.print();*/
@@ -199,7 +124,7 @@ fn global_shuffle(A: &UnsafeArray::<SortElement>,
         //println!("global shuffle 2c");
 
         // compute the prefix sum of the counts
-        inclusive_prefix_sum(&counts_lck, &world);
+        exclusive_prefix_sum(&counts_lck, &world);
 
         //println!("global shuffle 2d");
 
@@ -208,10 +133,10 @@ fn global_shuffle(A: &UnsafeArray::<SortElement>,
 
         // subtract the count from each entry in the inclusive
         // prefix sum to get the exclusive prefix sum
-        counts_lck.local_iter_mut().zip(copy_counts.local_iter()).for_each(move|(start,count)| { *start -= *count; }).block();
+        // counts_lck.local_iter_mut().zip(copy_counts.local_iter()).for_each(move|(start,count)| { *start -= *count; }).block();
 
-        /*println!("exclusive prefix sum is");
-        counts_lck.print();*/
+        // println!("exclusive prefix sum is");
+        // counts_lck.print();
 
         /*array_A.local_iter().zip(array_B.local_iter()).for_each(move|(elem_A,elem_B)| println!("PE: {my_pe} A: {elem_A} B: {elem_B}")).block();
         counts_lck.dist_iter_mut().for_each(move |elt|{
@@ -234,58 +159,62 @@ fn global_shuffle(A: &UnsafeArray::<SortElement>,
         // get fresh references to B and starts arrays
         let B_clone = B.clone();
         let starts_clone = starts.clone();
-        let _ = A.local_chunks(n_per_task)
-                 .enumerate().for_each_async(move |(tid,task_slice)| {
-                 // and get fresh references again
-                 let B_ = B_clone.clone();
-                 let starts_ = starts_clone.clone();
+        let _ = A
+            .local_chunks(n_per_task)
+            .enumerate()
+            .for_each_async(move |(tid, task_slice)| {
+                // and get fresh references again
+                let B_ = B_clone.clone();
+                let starts_ = starts_clone.clone();
 
-                 async move {
-                     //println!("tid {:?} got chunk {:?}", tid, task_slice);
-                     let mut this_task_starts;
+                async move {
+                    //println!("tid {:?} got chunk {:?}", tid, task_slice);
+                    let mut this_task_starts;
 
-                     // load the start positions for this task / chunk
-                     {
-                         let mut indices = vec![0; COUNTS_SIZE];
-                         for i in 0..COUNTS_SIZE {
-                             let glob_count_idx = i*n_tasks_total +
-                                                  my_pe*n_tasks_per_pe +
-                                                  tid;
+                    // load the start positions for this task / chunk
+                    {
+                        let mut indices = vec![0; COUNTS_SIZE];
+                        for i in 0..COUNTS_SIZE {
+                            let glob_count_idx = i * n_tasks_total + my_pe * n_tasks_per_pe + tid;
 
-                             indices[i] = glob_count_idx;
-                         }
-                         this_task_starts = starts_.batch_load(indices).await;
-                     }
+                            indices[i] = glob_count_idx;
+                        }
+                        this_task_starts = starts_.batch_load(indices).await;
+                    }
 
-                     // store the elements in the appropriate place
-                     // based upon the task starts
-                     {
-                         let size = task_slice.len();
-                         let mut indices = vec![0; size];
-                         for i in 0..size {
-                             let elt = &task_slice[i];
-                             let r = &mut this_task_starts[get_bucket(elt, digit)];
-                             indices[i] = *r as usize;
-                             *r += 1;
-                         }
+                    // store the elements in the appropriate place
+                    // based upon the task starts
+                    {
+                        // let size = task_slice.len();
+                        // let mut indices = vec![0; size];
+                        // for i in 0..size {
+                        //     let elt = &task_slice[i];
+                        //     let r = &mut this_task_starts[get_bucket(elt, digit)];
+                        //     indices[i] = *r as usize;
+                        //     *r += 1;
+                        // }
 
-                         //println!("tid {:?} chunk {:?} indices {:?}", tid, task_slice, indices);
+                        // //println!("tid {:?} chunk {:?} indices {:?}", tid, task_slice, indices);
 
-                         let _ = B_.batch_store(indices, task_slice).await;
+                        // let _ = B_.batch_store(indices, task_slice).await;
 
-                         /* it'd be cool if we could do this
-                         let indices_iter = task_slice.iter().enumerate()
-                                                      .map(|(i,elt)| {
-                             let r = &this_task_starts[get_bucket(elt, digit)];
-                             let idx = *r;
-                             *r += 1;
-                             r;
-                         });
-                         let _ = B_clone.batch_store(indices_iter,
-                                                     task_slice).spawn();*/
-                     }
-                 }
-               }).spawn();
+                        /* it'd be cool if we could do this */
+                        let mut indices_iter = task_slice.iter().enumerate().map(|(i, elt)| {
+                            let r = &mut this_task_starts[get_bucket(elt, digit)];
+                            let idx = *r;
+                            *r += 1;
+                            idx as usize
+                        });
+                        let _ = B_
+                            .batch_store(
+                                &mut indices_iter as &mut dyn Iterator<Item = usize>,
+                                task_slice,
+                            )
+                            .spawn();
+                    }
+                }
+            })
+            .spawn();
         //A.wait_all();
         //B.wait_all();
         world.wait_all();
@@ -295,16 +224,17 @@ fn global_shuffle(A: &UnsafeArray::<SortElement>,
 }
 
 // Sort the data in A, using B as scratch space.
-fn my_sort(A: &UnsafeArray<SortElement>,
-           B: &UnsafeArray<SortElement>,
-           world: &LamellarWorld,
-           n_per_task: usize) {
-
+fn my_sort(
+    A: &UnsafeArray<SortElement>,
+    B: &UnsafeArray<SortElement>,
+    world: &LamellarWorld,
+    n_per_task: usize,
+) {
     assert!(N_DIGITS % 2 == 0);
 
     for digit in (0..N_DIGITS).step_by(2) {
         global_shuffle(A, B, digit, world, n_per_task);
-        global_shuffle(B, A, digit+1, world, n_per_task);
+        global_shuffle(B, A, digit + 1, world, n_per_task);
     }
 }
 
@@ -317,9 +247,10 @@ fn main() {
     let world = lamellar::LamellarWorldBuilder::new().build();
     let my_pe = world.my_pe();
     let num_pes = world.num_pes();
-    let mut n: usize = args.get(1)
-                           .and_then(|s| s.parse::<usize>().ok())
-                           .unwrap_or_else(|| 64);
+    let mut n: usize = args
+        .get(1)
+        .and_then(|s| s.parse::<usize>().ok())
+        .unwrap_or_else(|| 64);
     // TODO: try multiplying n_tasks_per_pe by 2
     let n_tasks_per_pe: usize = world.num_threads_per_pe();
     let n_per_task: usize = usize::div_ceil(n, num_pes * n_tasks_per_pe);
@@ -329,14 +260,14 @@ fn main() {
     if my_pe == 0 {
         println!("hello from pe {} of {}", my_pe, num_pes);
         println!("there are {} tasks per pe", n_tasks_per_pe);
-        println!("n is {} n_per_pe is {} n_per_task is {}",
-                 n, n_per_pe, n_per_task);
+        println!(
+            "n is {} n_per_pe is {} n_per_task is {}",
+            n, n_per_pe, n_per_task
+        );
     }
 
-    let A_f = UnsafeArray::<SortElement>::new(world.team(), n,
-                                            Distribution::Block);
-    let B_f = UnsafeArray::<SortElement>::new(world.team(), n,
-                                            Distribution::Block);
+    let A_f = UnsafeArray::<SortElement>::new(world.team(), n, Distribution::Block);
+    let B_f = UnsafeArray::<SortElement>::new(world.team(), n, Distribution::Block);
     let A = A_f.block();
     let B = B_f.block();
 
@@ -345,10 +276,10 @@ fn main() {
     // set the input
     unsafe {
         /*input_array
-            .dist_iter_mut()
-            .enumerate()
-            .map_with(rng, |rng, | rng.sample(Standard))
-            .for_each(move |elem| *elem = (rand, idx));*/
+        .dist_iter_mut()
+        .enumerate()
+        .map_with(rng, |rng, | rng.sample(Standard))
+        .for_each(move |elem| *elem = (rand, idx));*/
 
         /* bad: same values for different tasks
         let rng = Pcg64::seed_from_u64((42 + my_pe) as u64);
@@ -388,39 +319,40 @@ fn main() {
                 });
             }
         });*/
-      /*
-        let loc_A = A.mut_local_data();
-        loc_A.par_chunks_mut(n_per_task)
-             .for_each(|slice| {
-                         println!("got chunk {:?}", slice);
-                       });
-      */
+        /*
+          let loc_A = A.mut_local_data();
+          loc_A.par_chunks_mut(n_per_task)
+               .for_each(|slice| {
+                           println!("got chunk {:?}", slice);
+                         });
+        */
 
-        let _ =
-          A.local_chunks_mut(n_per_task)
-             .enumerate().for_each(move|(tid,task_slice)| {
-                         //println!("tid {:?} got chunk {:?}", tid, task_slice);
-                         let seed = (42 + my_pe*n_tasks_per_pe + tid) as u64;
-                         let mut rng = Pcg64::seed_from_u64(seed);
-                         for (i, elt) in task_slice.iter_mut().enumerate() {
-                             let idx = (glob_start + tid*n_per_task + i) as u64;
-                             let r = rng.sample(StandardUniform);
-                             *elt = SortElement {key:r, val: idx};
-                         }
-                         //println!("tid {:?} ->  chunk {:?}", tid, task_slice);
-                       }).spawn();
+        let _ = A
+            .local_chunks_mut(n_per_task)
+            .enumerate()
+            .for_each(move |(tid, task_slice)| {
+                //println!("tid {:?} got chunk {:?}", tid, task_slice);
+                let seed = (42 + my_pe * n_tasks_per_pe + tid) as u64;
+                let mut rng = Pcg64::seed_from_u64(seed);
+                for (i, elt) in task_slice.iter_mut().enumerate() {
+                    let idx = (glob_start + tid * n_per_task + i) as u64;
+                    let r = rng.sample(StandardUniform);
+                    *elt = SortElement { key: r, val: idx };
+                }
+                //println!("tid {:?} ->  chunk {:?}", tid, task_slice);
+            })
+            .spawn();
         A.wait_all();
-
 
         // try local_iter_mut / local_chunks_mut
     }
 
     world.barrier();
+    // A.print();
 
     if my_pe == 0 {
         println!("elapsed rng time {}", time_rng.elapsed().as_secs_f64());
     }
-
 
     /*println!("Input for sort");
     A.print();*/
@@ -436,7 +368,29 @@ fn main() {
     A.print();*/
 
     if my_pe == 0 {
-        println!("elapsed sort time {}", time_rng.elapsed().as_secs_f64());
+        let time_sort = time_sort.elapsed().as_secs_f64();
+        println!(
+            "elapsed sort time {} M elems/s = {}",
+            time_sort,
+            (n as f64 / 1000000.0) / time_sort
+        );
+    }
+
+    // roughly check that the data is sorted
+    unsafe {
+        A.local_chunks(n_per_task)
+            .enumerate()
+            .for_each(move |(tid, task_slice)| {
+                let mut failed = false;
+                for i in 1..task_slice.len() {
+                    let prev = task_slice[i - 1].key;
+                    let curr = task_slice[i].key;
+                    failed |= prev > curr;
+                }
+                if failed {
+                    println!("pe {} task {} failed", my_pe, tid);
+                }
+            })
+            .block()
     }
 }
-
--- lamellar/lamellar_lsbsort/src/prefix_sum_impl.rs
+++ lamellar/lamellar_lsbsort/src/prefix_sum_impl.rs
@@ -1,70 +1,85 @@
 // This code is based on code provided by Ryan Friese.
 // Adding parallel prefix sum to Lamellar is on the TODO list.
 
-use lamellar::array::prelude::*;
 use lamellar::active_messaging::prelude::*;
-
+use lamellar::array::prelude::*;
 
 #[AmData]
-struct ApplyPePrefix{
+struct ApplyPePrefix {
     array: LocalLockArray<i64>,
     sum: i64,
 }
 
 #[am]
 impl LamellarAM for ApplyPePrefix {
-    async fn exec(&self) { 
-        self.array.write_local_chunks(1).await.for_each(move|mut chunk| {
-            
-            for i in chunk.iter_mut() {
-                *i += self.sum;
-            }
-        }).await;
+    async fn exec(&self) {
+        self.array
+            .write_local_chunks(1)
+            .await
+            .for_each(move |mut chunk| {
+                for i in chunk.iter_mut() {
+                    *i += self.sum;
+                }
+            })
+            .await;
     }
 }
 
-pub fn inclusive_prefix_sum(array: &LocalLockArray<i64>,
-                            world: &LamellarWorld) {
+pub fn inclusive_prefix_sum(array: &LocalLockArray<i64>, world: &LamellarWorld) {
     let my_pe = world.my_pe();
     let num_pes = world.num_pes();
 
     //an array to hold the sum of elements on each pe
     let pe_sums = AtomicArray::<i64>::new(world.team(), num_pes, Distribution::Block).block();
 
-    /*println!("computing prefix sum of");
-    array.print();*/
-   
+    // println!("computing prefix sum of");
+    // array.print();
+
     let chunk_size = array.num_elems_local() / world.num_threads_per_pe();
-    let local_chunk_sums = array.write_local_chunks(chunk_size).block().map(|mut chunk| {
-        let mut sum = 0;
-        for i in chunk.iter_mut() {
-            sum += *i;
-            *i = sum;
-        }
-        sum
-    
-    }).collect::<Vec<_>>(Distribution::Block).block();
+    let local_chunk_sums = array
+        .write_local_chunks(chunk_size)
+        .block()
+        .map(|mut chunk| {
+            let mut sum = 0;
+            for i in chunk.iter_mut() {
+                sum += *i;
+                *i = sum;
+            }
+            sum
+        })
+        .collect::<Vec<_>>(Distribution::Block)
+        .block();
 
     //calculate the local sum for each pe, and store it into local element of pe_sums
-    pe_sums.local_data().at(0).store(local_chunk_sums.iter().sum::<i64>());
+    pe_sums
+        .local_data()
+        .at(0)
+        .store(local_chunk_sums.iter().sum::<i64>());
 
     //calculate the local prefix sums
-    let _ = array.write_local_chunks(chunk_size).block().enumerate().for_each(move |(i,mut chunk)| {
-        let sum = local_chunk_sums[0..i].iter().sum::<i64>();
-        for i in chunk.iter_mut() {
-            *i += sum;
-        }
-    }).spawn(); //using a safe array we dont actually care if this finishes before we move on
+    let _ = array
+        .write_local_chunks(chunk_size)
+        .block()
+        .enumerate()
+        .for_each(move |(i, mut chunk)| {
+            let sum = local_chunk_sums[0..i].iter().sum::<i64>();
+            for i in chunk.iter_mut() {
+                *i += sum;
+            }
+        })
+        .block(); //using a safe array we dont actually care if this finishes before we move on
 
-    pe_sums.barrier();// ensure all pes have writen to pe_sums
+    pe_sums.barrier(); // ensure all pes have writen to pe_sums
 
+    // println!("temp  prefix sum array is");
+    // array.print();
     /*println!("in prefix sum array is");
     array.print();
     println!("pe_sums is");
     pe_sums.print();*/
 
     //calculate the pe prefix sums to reduce communication we only do this on pe 0
-    if my_pe == 0{
+    if my_pe == 0 {
         let mut sum = 0;
         for (pe, pe_sum) in pe_sums.onesided_iter().into_iter().enumerate() {
             // this following is a bit inefficient as have to send the indices for each batch_add but simple to use the array api
@@ -75,10 +90,15 @@ pub fn inclusive_prefix_sum(array: &LocalLockArray<i64>,
             //println!("pe {:?} applying sum {:?}", pe, sum);
 
             if sum != 0 {
-                let _ = world.exec_am_pe(pe,ApplyPePrefix {
-                    array: array.clone(),
-                    sum,
-                }).spawn();
+                let _ = world
+                    .exec_am_pe(
+                        pe,
+                        ApplyPePrefix {
+                            array: array.clone(),
+                            sum,
+                        },
+                    )
+                    .spawn();
             }
 
             sum += pe_sum;
@@ -87,6 +107,93 @@ pub fn inclusive_prefix_sum(array: &LocalLockArray<i64>,
     world.wait_all();
     world.barrier();
 
-    /*println!("prefix sum is");
-    array.print();*/
+    // println!("prefix sum is");
+    // array.print();
+}
+
+pub fn exclusive_prefix_sum(array: &LocalLockArray<i64>, world: &LamellarWorld) {
+    let my_pe = world.my_pe();
+    let num_pes = world.num_pes();
+
+    //an array to hold the sum of elements on each pe
+    let pe_sums = AtomicArray::<i64>::new(world.team(), num_pes, Distribution::Block).block();
+
+    // println!("computing prefix sum of");
+    // array.print();
+
+    let chunk_size = array.num_elems_local() / world.num_threads_per_pe();
+    let local_chunk_sums = array
+        .write_local_chunks(chunk_size)
+        .block()
+        .map(|mut chunk| {
+            let mut sum = 0;
+            for i in chunk.iter_mut() {
+                let temp = *i;
+                *i = sum;
+                (sum += temp);
+            }
+            sum
+        })
+        .collect::<Vec<_>>(Distribution::Block)
+        .block();
+
+    //calculate the local sum for each pe, and store it into local element of pe_sums
+    pe_sums
+        .local_data()
+        .at(0)
+        .store(local_chunk_sums.iter().sum::<i64>());
+
+    //calculate the local prefix sums
+    let _ = array
+        .write_local_chunks(chunk_size)
+        .block()
+        .enumerate()
+        .for_each(move |(i, mut chunk)| {
+            let sum = local_chunk_sums[0..i].iter().sum::<i64>();
+
+            for i in chunk.iter_mut() {
+                let temp = sum;
+                *i += temp;
+            }
+        })
+        .spawn(); //using a safe array we dont actually care if this finishes before we move on
+
+    pe_sums.barrier(); // ensure all pes have writen to pe_sums
+
+    // println!("temp  prefix sum array is");
+    // array.print();
+    /*println!("pe_sums is");
+    pe_sums.print();*/
+
+    //calculate the pe prefix sums to reduce communication we only do this on pe 0
+    if my_pe == 0 {
+        let mut sum = 0;
+        for (pe, pe_sum) in pe_sums.onesided_iter().into_iter().enumerate() {
+            // this following is a bit inefficient as have to send the indices for each batch_add but simple to use the array api
+            // let mut  pe_indices = array.first_global_index_for_pe(pe).unwrap()..=array.last_global_index_for_pe(pe).unwrap();
+            // let _ = array.batch_add(&mut pe_indices as &mut dyn Iterator<Item=i64>, sum).spawn();
+
+            //alteratively we can do this with an AM with much less overhead
+            //println!("pe {:?} applying sum {:?}", pe, sum);
+
+            if sum != 0 {
+                let _ = world
+                    .exec_am_pe(
+                        pe,
+                        ApplyPePrefix {
+                            array: array.clone(),
+                            sum,
+                        },
+                    )
+                    .spawn();
+            }
+
+            sum += pe_sum;
+        }
+    }
+    world.wait_all();
+    world.barrier();
+
+    // println!("prefix sum is");
+    // array.print();
 }
